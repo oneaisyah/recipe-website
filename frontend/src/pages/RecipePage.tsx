4c4ead69bdd6aa902a0e564c14796a9c93a34a15
oneaisyah <wanaisyahwan2@gmail.com>
2023-11-02 14:09:51 +0800

    navigate to individual recipe page

--- frontend/src/pages/RecipePage.tsx
+++ frontend/src/pages/RecipePage.tsx
@@ -1,14 +1,17 @@
 import Navbar from "../components/Navbar/Navbar";
 import Recipe from "../components/Recipe/Recipe";
 import dummyRecipe from "../data/recipes.json"
+import { useParams } from 'react-router-dom';
 
 function RecipePage() {
+    const { id = '' } = useParams();
+    const parsedId = parseInt(id, 10);
+    const selectedRecipe = dummyRecipe.find(recipe => recipe.id === parsedId);
+    
     return (
         <div>
             <Navbar />
-            <Recipe
-                recipe={dummyRecipe[1]}
-            />
+            {selectedRecipe && <Recipe recipe={selectedRecipe} />}
         </div>
     )
 }
