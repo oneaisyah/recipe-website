4d45eda3a9bffbe80ebd82e9a51207ebb2d8830f
oneaisyah <wanaisyahwan2@gmail.com>
2023-11-02 10:09:51 +0800

    inital recipe page layout

--- frontend/src/pages/RecipePage.tsx
+++ frontend/src/pages/RecipePage.tsx
@@ -1,6 +1,15 @@
+import Navbar from "../components/Navbar/Navbar";
+import Recipe from "../components/Recipe/Recipe";
+import dummyRecipe from "../data/recipes.json"
+
 function RecipePage() {
     return (
-        <div>recipe</div>
+        <div>
+            <Navbar />
+            <Recipe
+                recipe={dummyRecipe[1]}
+            />
+        </div>
     )
 }
 
