19d2a79e91d815a5c378cf9bfb33d892cb027a9b
oneaisyah <wanaisyahwan2@gmail.com>
2023-11-06 16:00:15 +0800

    update instructions

--- backend/src/controllers/instructionController.ts
+++ backend/src/controllers/instructionController.ts
@@ -21,3 +21,59 @@ export const addInstructionToRecipe = async (req: Request, res: Response): Promi
         res.status(500).json({ message: 'Server Error' });
     }
 };
+
+export const updateInstruction = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const [updated] = await Instruction.update(req.body, {
+            where: { id: Number(id) },
+        });
+        if (updated) {
+            const updatedInstruction = await Instruction.findByPk(id);
+            res.status(200).json({ instruction: updatedInstruction });
+        } else {
+            throw new Error('Instruction not found');
+        }
+    } catch (error: any) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const deleteInstruction = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { recipeId } = req.params;
+        const deleted = await Instruction.destroy({
+            where: { id: Number(recipeId) },
+        });
+        if (deleted) {
+            res.status(204).send('Instruction deleted');
+        } else {
+            throw new Error('Instruction not found');
+        }
+    } catch (error: any) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const reorderInstructions = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { recipeId, instructions } = req.body;
+        const updatedInstructions = await Promise.all(instructions.map(async (instruction: any) => {
+            const [updated] = await Instruction.update(instruction, {
+                where: { id: Number(instruction.id) },
+            });
+            if (updated) {
+                const updatedInstruction = await Instruction.findByPk(instruction.id);
+                return updatedInstruction;
+            } else {
+                throw new Error('Instruction not found');
+            }
+        }));
+        res.status(200).json({ instructions: updatedInstructions });
+    } catch (error: any) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+};
--- backend/src/routes/instructionRoute.ts
+++ backend/src/routes/instructionRoute.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { addInstructionToRecipe, getInstructionsByRecipeId } from "../controllers/instructionController";
+import { addInstructionToRecipe, deleteInstruction, getInstructionsByRecipeId, updateInstruction } from "../controllers/instructionController";
 
 const router = express.Router();
 
 router.get('/instructions/:recipeId', getInstructionsByRecipeId);
 router.post('/instructions', addInstructionToRecipe);
+router.put('/instructions/:id', updateInstruction);
+router.delete('/instructions/:id', deleteInstruction);
+router.put('/instructions/reorder/:recipeId', updateInstruction);
 
 export { router as instructionRoutes };
